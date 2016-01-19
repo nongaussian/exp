f53cd6de731182c6cbb83dfe368d029ff3de0e53
Younghoon Kim <nongaussian@gmail.com>
2016-01-19 23:31:20 +0900

    minor

--- infosource/src/common/Node.java
+++ infosource/src/common/Node.java
@@ -1,7 +1,18 @@
 package common;
 
 public class Node {
-	public Node () {
-		
+	public int id;
+	public int[] neighbors = null;
+	public int idx = 0;
+	
+	public Node (int id) { this.id = id; }
+	
+	public void init_neighbor (int size) {
+		neighbors = new int[size];
+		idx = 0;
+	}
+	
+	public void add_neighbor (int n) {
+		neighbors[idx++] = n;
 	}
 }
--- infosource/src/common/UndirGraph.java
+++ infosource/src/common/UndirGraph.java
@@ -2,10 +2,31 @@ package common;
 
 public class UndirGraph {
 	private Node root = null;
+	private Node[] nodes = null;
 	
-	public UndirGraph () {
-		
+	public UndirGraph (int n_nodes) {
+		nodes = new Node[n_nodes];
+		for (int i=0; i<n_nodes; i++) {
+			nodes[i] = new Node (i);
+		}
 	}
+	
+	/*
+	 * Graph loading
+	 */
+	
+	public void set_neighbor_size (int n, int size) {
+		nodes[n].init_neighbor(size);
+	}
+
+	public void add_edge(int n1, int n2) {
+		nodes[n1].add_neighbor(n2);
+		nodes[n2].add_neighbor(n1);
+	}
+	
+	/*
+	 * Graph traverse
+	 */
 	
 	// in advance to the traverse of graph, we set the root node
 	public void set_root() { 
