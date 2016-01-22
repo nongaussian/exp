1f9ef992729848960f39188d440856b0659558ba
Younghoon Kim <nongaussian@gmail.com>
2016-01-23 02:10:08 +0900

    minor

--- infosource/src/common/Graph.java
+++ infosource/src/common/Graph.java
@@ -1,10 +1,14 @@
 package common;
 
-public class UndirGraph {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class Graph {
 	private Node root = null;
 	private Node[] nodes = null;
 	
-	public UndirGraph (int n_nodes) {
+	public Graph (int n_nodes) {
 		nodes = new Node[n_nodes];
 		for (int i=0; i<n_nodes; i++) {
 			nodes[i] = new Node (i);
@@ -36,5 +40,22 @@ public class UndirGraph {
 	// traverse the graph with BFS & compute log-likelihood of each case
 	public void traverse (Traveler t) {
 		
+	}
+	
+	/*
+	 * Data load
+	 */
+	
+	public void read_from_file (String filename) {
+		String  thisLine = null;
+		try{
+			// open input stream test.txt for reading purpose.
+			BufferedReader br = new BufferedReader(new FileReader(new File(filename)));
+			while ((thisLine = br.readLine()) != null) {
+		        System.out.println(thisLine);
+		     }       
+		  }catch(Exception e){
+		     e.printStackTrace();
+		  }
 	}
 }
